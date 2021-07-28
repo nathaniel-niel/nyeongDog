e1a138a0c91467f821a43909d2f3f8123a397590
nathaniel andrian <nathaniel.task@gmail.com>
2021-07-28 21:34:40 +0700

    initalFile
    set initialFile

---
+++ Annyeong Dog/Vendor/Network/DataManipulation.swift
@@ -0,0 +1,8 @@
+//
+//  DataManipulation.swift
+//  Annyeong Dog
+//
+//  Created by Nathaniel Andrian on 28/07/21.
+//
+
+import Foundation
---
+++ Annyeong Dog/Vendor/Util/Helper.swift
@@ -0,0 +1,8 @@
+//
+//  Helper.swift
+//  Annyeong Dog
+//
+//  Created by Nathaniel Andrian on 28/07/21.
+//
+
+import Foundation
