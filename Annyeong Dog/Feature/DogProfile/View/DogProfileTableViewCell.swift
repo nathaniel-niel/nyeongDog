8465f01b1cf4b3e13331096929e20788ac7ae25c
Pieter Yonathan <pieteryonathan20@gmail.com>
2021-07-29 12:07:40 +0700

    commit punya piet

---
+++ Annyeong Dog/Feature/DogProfile/View/DogProfileTableViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  DogProfileTableViewCell.swift
+//  Annyeong Dog
+//
+//  Created by Pieter Yonathan on 29/07/21.
+//
+
+import UIKit
+
+class DogProfileTableViewCell: UITableViewCell {
+
+    //IBOutlet
+    @IBOutlet weak var dogProfileView: UIView!
+    @IBOutlet weak var dogPicture: UIImageView!
+    @IBOutlet weak var dogName: UILabel!
+    @IBOutlet weak var dogWeight: UILabel!
+    @IBOutlet weak var dogGender: UILabel!
+    @IBOutlet weak var detailButton: UIButton!
+    
+}
