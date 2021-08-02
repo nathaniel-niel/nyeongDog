73f1041e2be9bd88f5392f5cb3899e40174a479e
Novi Gunawan <novgunawan07@gmail.com>
2021-08-02 16:39:10 +0700

    Add sign in folder

---
+++ trial-AnnyeongDog/Feature/SignIn/View/XIB/SignInViewExtension.swift
@@ -0,0 +1,17 @@
+//
+//  SignInViewExtension.swift
+//  trial-AnnyeongDog
+//
+//  Created by Novi Gunawan on 02/08/21.
+//
+
+import Foundation
+import UIKit
+
+extension UIView {
+    func loadViewFromNib(nibName: String) -> UIView? {
+        let bundle = Bundle(for: type(of: self))
+        let nib = UINib(nibName: nibName, bundle: bundle)
+        return nib.instantiate(withOwner: self, options: nil).first as? UIView
+    }
+}
