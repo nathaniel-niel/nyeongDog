992a38f743a456f545fd8c73e508331947b65690
Novi Gunawan <novgunawan07@gmail.com>
2021-08-05 13:38:10 +0700

    Create Chat's UI

---
+++ trial-AnnyeongDog/Feature/Chat/Model/Messages.swift
@@ -0,0 +1,13 @@
+//
+//  Messages.swift
+//  trial-AnnyeongDog
+//
+//  Created by Novi Gunawan on 05/08/21.
+//
+
+import Foundation
+
+struct Messages {
+    let sender: String
+    let body: String
+}
---
+++ trial-AnnyeongDog/Feature/Chat/View/ChatViewCell.swift
@@ -0,0 +1,30 @@
+//
+//  ChatViewCell.swift
+//  trial-AnnyeongDog
+//
+//  Created by Novi Gunawan on 05/08/21.
+//
+
+import UIKit
+
+class ChatViewCell: UITableViewCell {
+
+    @IBOutlet weak var messageBubble: UIView!
+    @IBOutlet weak var messageLabel: UILabel!
+    static let identifier = "chatViewCell"
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        messageBubble.layer.cornerRadius = messageBubble.frame.height/5
+    }
+    static func nib() -> UINib {
+        return UINib(nibName: "ChatViewCell", bundle: nil)
+        
+        
+    }
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
