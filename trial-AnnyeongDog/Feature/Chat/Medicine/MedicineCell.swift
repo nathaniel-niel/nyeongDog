84fd714d089d180c91806efb29b60072956615a9
Novi Gunawan <novgunawan07@gmail.com>
2021-08-05 17:28:36 +0700

    Create prescription ui view [not finished]

---
+++ trial-AnnyeongDog/Feature/Chat/Medicine/MedicineCell.swift
@@ -0,0 +1,23 @@
+//
+//  MedicineCell.swift
+//  trial-AnnyeongDog
+//
+//  Created by Novi Gunawan on 05/08/21.
+//
+
+import UIKit
+
+class MedicineCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ trial-AnnyeongDog/Feature/Chat/Prescription/PrescriptionViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  PrescriptionViewCell.swift
+//  trial-AnnyeongDog
+//
+//  Created by Novi Gunawan on 05/08/21.
+//
+
+import UIKit
+
+class PrescriptionViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
