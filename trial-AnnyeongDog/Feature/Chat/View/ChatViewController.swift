18cd57e232e74d1e751b978128c50d5b2a43d77b
zkake <ahmad.azka.kusuma@mail.ugm.ac.id>
2021-08-06 20:09:28 +0700

    Chat are connected to Databse Firebase Write

--- trial-AnnyeongDog/Feature/Chat/View/ChatViewController.swift
+++ trial-AnnyeongDog/Feature/Chat/View/ChatViewController.swift
@@ -22,14 +22,11 @@ class ChatViewController: UIViewController {
     let db = Firestore.firestore()
     var dbCollection = DatabaseCollectionName()
     
-    var messages: [Messages] = [
-        Messages(sender: "novi", body: "Hey"),
-        Messages(sender: "vivi", body: "Hello nov")
-    ]
+    var messages: [Messages] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         setup()
         
         //MARK: -Register xib cell
@@ -40,16 +37,18 @@ class ChatViewController: UIViewController {
         messageTextField.delegate = self
         messageTextField.autocorrectionType = .no
         
+        getMessage()
+        
         //MARK: -Keyboard Notification Center
         let center: NotificationCenter = NotificationCenter.default;
-//        center.addObserver(self, selector: #selector(keyboardDidShow(notification:)), name: UIResponder.keyboardWillShowNotification, object: nil)
-//        center.addObserver(self, selector: #selector(keyboardWillHide(notification:)), name: UIResponder.keyboardWillHideNotification, object: nil)
-//        center.addObserver(self, selector: #selector(self.keyboardNotification(notification: )), name: UIResponder.keyboardWillChangeFrameNotification, object: nil)
-//        center.addObserver(self, selector: #selector(keyboardWillChange(notification: )), name: UIResponder.keyboardWillShowNotification, object: nil)
-//        center.addObserver(self, selector: #selector(keyboardWillChange(notification: )), name: UIResponder.keyboardWillHideNotification, object: nil)
+        //        center.addObserver(self, selector: #selector(keyboardDidShow(notification:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+        //        center.addObserver(self, selector: #selector(keyboardWillHide(notification:)), name: UIResponder.keyboardWillHideNotification, object: nil)
+        //        center.addObserver(self, selector: #selector(self.keyboardNotification(notification: )), name: UIResponder.keyboardWillChangeFrameNotification, object: nil)
+        //        center.addObserver(self, selector: #selector(keyboardWillChange(notification: )), name: UIResponder.keyboardWillShowNotification, object: nil)
+        //        center.addObserver(self, selector: #selector(keyboardWillChange(notification: )), name: UIResponder.keyboardWillHideNotification, object: nil)
         center.addObserver(self, selector: #selector(keyboardWillChange(notification: )), name: UIResponder.keyboardWillChangeFrameNotification, object: nil)
-//
-//
+        //
+        //
     }
     
     deinit {
@@ -60,39 +59,39 @@ class ChatViewController: UIViewController {
         view.frame.origin.y = -300
     }
     
-//    @objc func keyboardNotification(notification: NSNotification) {
-//        if let userInfo = notification.userInfo {
-//            let endFrame = (userInfo[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue
-//            let duration:TimeInterval = (userInfo[UIResponder.keyboardAnimationDurationUserInfoKey] as! NSNumber).doubleValue ?? 0
-//            let animationCurveRawNSN = userInfo[UIResponder.keyboardAnimationCurveUserInfoKey] as? NSNumber
-//            let animationCurveRaw = animationCurveRawNSN?.uintValue ?? UIView.AnimationOptions.curveEaseInOut.rawValue
-//            let animationCurve: UIView.AnimationOptions = UIView.AnimationOptions(rawValue: animationCurveRaw)
-//
-//            UIView.animate(withDuration: duration, delay: TimeInterval(0), options: animationCurve, animations: {self.view.layoutIfNeeded()}, completion: nil)
-//        }
-//    }
-//    //MARK: -Function Keyboard is Called
-//    @objc func keyboardDidShow(notification: Notification) {
-//        let info: NSDictionary = notification.userInfo! as NSDictionary
-//        let keyboardSize = (info[UIResponder.keyboardFrameBeginUserInfoKey] as! NSValue).cgRectValue
-//        let keyboardY = self.view.frame.size.height - keyboardSize.height
-//
-//        let editingTextFieldY: CGFloat! = self.messageTextField.frame.origin.y
-//        if editingTextFieldY > keyboardY - 60 {
-//            UIView.animate(withDuration: 0.25, delay: 0.0, options: UIView.AnimationOptions.curveEaseIn, animations: {
-//                self.view.frame = CGRect(x: 0, y: self.view.frame.origin.y, width: self.view.bounds.width, height: self.view.bounds.height)
-//            }, completion: nil)
-//        }
-//
-//    }
-//    //MARK: -Function Keyboard is Dismissed
-//    @objc func keyboardWillHide(notification: Notification) {
-//        UIView.animate(withDuration: 0.25, delay: 0.0, options: UIView.AnimationOptions.curveEaseIn, animations: {
-//            self.view.frame = CGRect(x: 0, y: 0, width: self.view.bounds.width, height: self.view.bounds.height)
-//        }, completion: nil)
-//    }
+    //    @objc func keyboardNotification(notification: NSNotification) {
+    //        if let userInfo = notification.userInfo {
+    //            let endFrame = (userInfo[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue
+    //            let duration:TimeInterval = (userInfo[UIResponder.keyboardAnimationDurationUserInfoKey] as! NSNumber).doubleValue ?? 0
+    //            let animationCurveRawNSN = userInfo[UIResponder.keyboardAnimationCurveUserInfoKey] as? NSNumber
+    //            let animationCurveRaw = animationCurveRawNSN?.uintValue ?? UIView.AnimationOptions.curveEaseInOut.rawValue
+    //            let animationCurve: UIView.AnimationOptions = UIView.AnimationOptions(rawValue: animationCurveRaw)
+    //
+    //            UIView.animate(withDuration: duration, delay: TimeInterval(0), options: animationCurve, animations: {self.view.layoutIfNeeded()}, completion: nil)
+    //        }
+    //    }
+    //    //MARK: -Function Keyboard is Called
+    //    @objc func keyboardDidShow(notification: Notification) {
+    //        let info: NSDictionary = notification.userInfo! as NSDictionary
+    //        let keyboardSize = (info[UIResponder.keyboardFrameBeginUserInfoKey] as! NSValue).cgRectValue
+    //        let keyboardY = self.view.frame.size.height - keyboardSize.height
+    //
+    //        let editingTextFieldY: CGFloat! = self.messageTextField.frame.origin.y
+    //        if editingTextFieldY > keyboardY - 60 {
+    //            UIView.animate(withDuration: 0.25, delay: 0.0, options: UIView.AnimationOptions.curveEaseIn, animations: {
+    //                self.view.frame = CGRect(x: 0, y: self.view.frame.origin.y, width: self.view.bounds.width, height: self.view.bounds.height)
+    //            }, completion: nil)
+    //        }
+    //
+    //    }
+    //    //MARK: -Function Keyboard is Dismissed
+    //    @objc func keyboardWillHide(notification: Notification) {
+    //        UIView.animate(withDuration: 0.25, delay: 0.0, options: UIView.AnimationOptions.curveEaseIn, animations: {
+    //            self.view.frame = CGRect(x: 0, y: 0, width: self.view.bounds.width, height: self.view.bounds.height)
+    //        }, completion: nil)
+    //    }
     
-   
+    
     //MARK: -Setup navigation and tabbar
     func setup() {
         //MARK: Customize Back Button
@@ -131,6 +130,33 @@ class ChatViewController: UIViewController {
         
     }
     
+    func getMessage(){
+
+        db.collection(dbCollection.collectionName).getDocuments { (querrySnapshot, error) in
+            if let e = error{
+                print(e)
+            }else{
+                if let snapshotDocuments = querrySnapshot?.documents{
+                    for doc in snapshotDocuments{
+                       let data = doc.data()
+                        if let sender = data[self.dbCollection.senderField] as? String, let messageBody = data[self.dbCollection.bodyField] as? String{
+                            let newMessage = Messages(sender: sender, body: messageBody)
+                            self.messages.append(newMessage)
+                            
+                            DispatchQueue.main.async {
+                                self.chatTableView.reloadData()
+                            }
+                           
+                            
+                        }
+                    }
+                    
+                }
+            }
+        }
+        
+    }
+    
     
     //MARK: Back Button Logic
     @objc func didTapBackButton() {
@@ -151,18 +177,19 @@ extension ChatViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = chatTableView.dequeueReusableCell(withIdentifier: ChatViewCell.identifier, for: indexPath) as! ChatViewCell
         
-        switch indexPath.row {
-        
-        case 0 :
-            cell.messageLabel.text = messages[indexPath.row].body
-        case 1 :
-            cell.messageLabel.text = messages[indexPath.row].body
-        case 2 :
-            let prescriptionCell = chatTableView.dequeueReusableCell(withIdentifier: PrescriptionViewCell.identifier) as! PrescriptionViewCell
-            
-        default :
-            print("Error")
-        }
+        cell.messageLabel.text = messages[indexPath.row].body
+//        switch indexPath.row {
+//
+//        case 0 :
+//            cell.messageLabel.text = messages[indexPath.row].body
+//        case 1 :
+//            cell.messageLabel.text = messages[indexPath.row].body
+//        case 2 :
+//            let prescriptionCell = chatTableView.dequeueReusableCell(withIdentifier: PrescriptionViewCell.identifier) as! PrescriptionViewCell
+//
+//        default :
+//            print("Error")
+//        }
         return cell
     }
     
@@ -172,10 +199,10 @@ extension ChatViewController: UITextFieldDelegate {
     
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         //MARK: If return button is tapped, keyboard will be dismissed
-//        self.view.endEditing(true)
+        //        self.view.endEditing(true)
         textField.resignFirstResponder()
         print(textField.text)
         return true
     }
-   
+    
 }
