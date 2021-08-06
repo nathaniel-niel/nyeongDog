fd1c81c545dc93c6303faa9272ac0808027d455c
Novi Gunawan <novgunawan07@gmail.com>
2021-08-06 16:30:04 +0700

    Add comments for the ChatView

--- trial-AnnyeongDog/Feature/Chat/View/ChatViewController.swift
+++ trial-AnnyeongDog/Feature/Chat/View/ChatViewController.swift
@@ -13,6 +13,7 @@ class ChatViewController: UIViewController {
     @IBOutlet weak var cameraButton: UIButton!
     @IBOutlet weak var sendButton: UIButton!
     @IBOutlet weak var messageTextField: UITextField!
+    @IBOutlet weak var chatView: UIView!
     
     
     //MARK: - Calling Facetime Methods
@@ -24,54 +25,60 @@ class ChatViewController: UIViewController {
     ]
     override func viewDidLoad() {
         super.viewDidLoad()
+
         setup()
         
         //MARK: -Register xib cell
         chatTableView.register(ChatViewCell.nib(), forCellReuseIdentifier: ChatViewCell.identifier)
         chatTableView.register(PrescriptionViewCell.nib(), forCellReuseIdentifier: PrescriptionViewCell.identifier)
         
+        //MARK: -Message TextField Configuration
         messageTextField.delegate = self
+        messageTextField.autocorrectionType = .no
         
-//        let center: NotificationCenter = NotificationCenter.default;
-//        center.addObserver(self, selector: #selector(keyboardDidShow(notification:)), name: NSNotification.Name.UIKeyboardWillShow, object: nil)
-//        center.addObserver(self, selector: #selector(keyboardWillHide(notification:)), name: NSNotification.Name.UIKeyboardWillHide, object: nil)
+        //MARK: -Keyboard Notification Center
+        let center: NotificationCenter = NotificationCenter.default;
+        center.addObserver(self, selector: #selector(keyboardDidShow(notification:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+        center.addObserver(self, selector: #selector(keyboardWillHide(notification:)), name: UIResponder.keyboardWillHideNotification, object: nil)
     }
     
-//    func keyboardDidShow(notification: Notification) {
-//        let info: NSDictionary = notification.userInfo! as NSDictionary
-//        let keyboardSize = (info[UIKeyboardFrameBeginUserInfoKey] as! NSValue).cgRectValue
-//        let keyboardY = self.view.frame.size.height - keyboardSize.height
-//        
-//        let editingTextFieldY: CGFloat! = self.messageTextField.frame.origin.y
-//        
-//        if editingTextFieldY > keyboardY - 60 {
-//            UIView.animate(withDuration: 0.25, delay: 0.0, options: UIView.AnimationOption, animations: <#T##() -> Void#>, completion: <#T##((Bool) -> Void)?##((Bool) -> Void)?##(Bool) -> Void#>)
-//        }
-//        
-//    }
     
-//    func keyboardWillHide(notification: Notification) {
-//        UIView.animate(withDuration: 0.25, delay: 0.0, options: UIView.AnimationOptions.curveEaseIn, animations: {
-//            self.view.frame = CGRect(x: 0, y: 0, width: self.view.bounds.width, height: self.view.bounds.height)
-//        }, completion: nil)
-//    }
+    //MARK: -Function Keyboard is Called
+    @objc func keyboardDidShow(notification: Notification) {
+        let info: NSDictionary = notification.userInfo! as NSDictionary
+        let keyboardSize = (info[UIResponder.keyboardFrameBeginUserInfoKey] as! NSValue).cgRectValue
+        let keyboardY = self.view.frame.size.height - keyboardSize.height
+        
+        let editingTextFieldY: CGFloat! = self.chatView.frame.origin.y
+        if editingTextFieldY > keyboardY - 60 {
+            UIView.animate(withDuration: 0.25, delay: 0.0, options: UIView.AnimationOptions.curveEaseIn, animations: {
+                self.view.frame = CGRect(x: 0, y: self.view.frame.origin.y, width: self.view.bounds.width, height: self.view.bounds.height)
+            }, completion: nil)
+        }
+        
+    }
+    //MARK: -Function Keyboard is Dismissed
+    @objc func keyboardWillHide(notification: Notification) {
+        UIView.animate(withDuration: 0.25, delay: 0.0, options: UIView.AnimationOptions.curveEaseIn, animations: {
+            self.view.frame = CGRect(x: 0, y: 0, width: self.view.bounds.width, height: self.view.bounds.height)
+        }, completion: nil)
+    }
     
-    
+   
+    //MARK: -Setup navigation and tabbar
     func setup() {
-        //MARK: -Customize Back Button
+        //MARK: Customize Back Button
         let backButton = UIBarButtonItem(image: UIImage(systemName: "chevron.left"), style: .plain, target: self, action: #selector(didTapBackButton))
         
-        //MARK: -Customize Vet Profile
+        //MARK: Customize Vet Profile
         let profileVet = UIBarButtonItem(customView: VetProfile.init())
-        
         self.navigationItem.leftBarButtonItems = [backButton, profileVet]
         
-        //MARK: -Customize Video Call Button
+        //MARK: Customize Video Call Button
         self.navigationItem.rightBarButtonItem = UIBarButtonItem(image: UIImage(systemName: "video"), style: .plain, target: self, action: #selector(didTapVideoButton))
-    
-        
+     
         self.navigationItem.largeTitleDisplayMode = .never
-        //MARK: -Hide tab bar in chat
+        //MARK: Hide tab bar in chat
         self.tabBarController?.tabBar.isHidden = true
     }
     
@@ -82,15 +89,13 @@ class ChatViewController: UIViewController {
     
     //MARK: -Logic Video Call Button
     @objc func didTapVideoButton() {
-        
         facetimeCall.fetchFacetime()
-        
     }
 }
 
 extension ChatViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return messages.count+1
+        return messages.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -115,6 +120,7 @@ extension ChatViewController: UITableViewDelegate, UITableViewDataSource {
 extension ChatViewController: UITextFieldDelegate {
     
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        //MARK: If return button is tapped, keyboard will be dismissed
         textField.resignFirstResponder()
         print(textField.text)
         return true
