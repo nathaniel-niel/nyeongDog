60fe5226ee440ff516c434e9dd02cbf38066a122
Pieter Yonathan <pieteryonathan20@gmail.com>
2021-08-09 17:05:45 +0700

    benerin bug

--- trial-AnnyeongDog/Feature/ListVet/Model/Vetdummydata.swift
+++ trial-AnnyeongDog/Feature/ListVet/Model/Vetdummydata.swift
@@ -12,10 +12,10 @@ class VetDummyData{
 //    var data = [VetListModel(vetName: "Drh. Stefan", expYears: "12", price: "Rp. 35.000", rating: "4"), VetListModel(vetName: "Drh. Anisa", expYears: "10", price: "Rp. 35.000", rating: "5"), VetListModel(vetName: "Drh. Ikwan", expYears: "12", price: "Rp. 35.000", rating: "4"), VetListModel(vetName: "Drh. Bambang", price: "Rp. 35.000", expYears: "10", rating: "5"), VetListModel(vetName: "Drh. Budi", price: "Rp. 35.000", expYears: "12", rating: "4"),VetListModel(vetName: "Drh. Azka", price: "Rp. 35.000", expYears: "10", rating: "5"),VetListModel(vetName: "Drh. Karin", expYears: "12", price: "Rp. 35.000", rating: "4")]
 //
     var data = [
-        VetListModel(vetName: "Drh. Stefan", expYears: "12", timeSlot: "Morning", clinicName: "abc", strvNumber: "xxx xxx xxx", rating: "5", price: "Rp. 35.000", alumnus: "Univ xxx"),
-        VetListModel(vetName: "Drh. Ikwan", expYears: "12", timeSlot: "Morning", clinicName: "abc", strvNumber: "xxx xxx xxx", rating: "5", price: "Rp. 35.000", alumnus: "Univ xxx"),
-        VetListModel(vetName: "Drh. Budi", expYears: "12", timeSlot: "Morning", clinicName: "abc", strvNumber: "xxx xxx xxx", rating: "5", price: "Ro. 35.000", alumnus: "Univ xxx"),
-        VetListModel(vetName: "Drh. Anisa", expYears: "12", timeSlot: "Morning", clinicName: "abc", strvNumber: "xxx xxx xxx", rating: "5", price: "Ro. 35.000", alumnus: "Univ xxx")
+        VetListModel(vetName: "Drh. Stefan", expYears: "12", timeSlot: "Morning", clinicName: "Klinik Bahagia", strvNumber: "534 435 535", rating: "5", price: "Rp. 35.000", alumnus: "Univ xxx"),
+        VetListModel(vetName: "Drh. Ikwan", expYears: "4", timeSlot: "Afternoon", clinicName: "Klinik Permata Indah", strvNumber: "534 534 534", rating: "4", price: "Rp. 85.000", alumnus: "Univ xxx"),
+        VetListModel(vetName: "Drh. Budi", expYears: "6", timeSlot: "Evening", clinicName: "Klinik Emas Murni", strvNumber: "543 646 765", rating: "5", price: "Rp. 10.000", alumnus: "Univ xxx"),
+        VetListModel(vetName: "Drh. Anisa", expYears: "8", timeSlot: "Evening", clinicName: "Klinik Berlian Terang", strvNumber: "456 987 658", rating: "4", price: "Rp. 45.000", alumnus: "Univ xxx")
     
     
     
