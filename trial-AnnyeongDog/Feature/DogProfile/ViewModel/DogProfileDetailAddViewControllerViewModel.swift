9a0d33346094d2f4f56ad90193b20da3f4577263
zkake <ahmad.azka.kusuma@mail.ugm.ac.id>
2021-08-19 00:12:57 +0700

    refactoring regarding to the Notion

---
+++ trial-AnnyeongDog/Feature/DogProfile/ViewModel/DogProfileDetailAddViewControllerViewModel.swift
@@ -0,0 +1,17 @@
+//
+//  DogProfileDetailAddViewControllerViewModel.swift
+//  trial-AnnyeongDog
+//
+//  Created by Azka Kusuma on 18/08/21.
+//
+
+import UIKit
+
+struct DogProfileDetailAddViewControllerViewModel {
+    
+    func imagePicker(sourceType: UIImagePickerController.SourceType) -> UIImagePickerController {
+        let imagePicker = UIImagePickerController()
+        imagePicker.sourceType = sourceType
+        return imagePicker
+    }
+}
