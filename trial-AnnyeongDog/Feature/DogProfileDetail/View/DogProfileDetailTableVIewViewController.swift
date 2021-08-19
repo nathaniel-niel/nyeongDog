2a9e4fd1c1877b3297b6f58f3e5c8ba98122966f
zkake <ahmad.azka.kusuma@mail.ugm.ac.id>
2021-08-19 12:34:47 +0700

    tableview added

---
+++ trial-AnnyeongDog/Feature/DogProfileDetail/View/DogProfileDetailTableVIewViewController.swift
@@ -0,0 +1,21 @@
+//
+//  DogProfileDetailTableVIewViewController.swift
+//  trial-AnnyeongDog
+//
+//  Created by Azka Kusuma on 19/08/21.
+//
+
+import UIKit
+
+class DogProfileDetailTableVIewViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+  
+
+}
