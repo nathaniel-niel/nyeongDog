b4e220a2fc5777ce81cc730fcdb90b5b957f08d5
zkake <ahmad.azka.kusuma@mail.ugm.ac.id>
2021-08-24 22:45:05 +0700

    XiB

---
+++ trial-AnnyeongDog/Feature/DogProfileDetail/UI/MedicalRecordsButton/MedicalRecordsButton.swift
@@ -0,0 +1,23 @@
+//
+//  MedicalRecordsButton.swift
+//  trial-AnnyeongDog
+//
+//  Created by Azka Kusuma on 24/08/21.
+//
+
+import UIKit
+
+class MedicalRecordsButton: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
