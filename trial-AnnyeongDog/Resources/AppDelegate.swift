326560e100c3c9e0e3f61f907f7f25c380dd687a
Nathaniel Andrian <nathaniel.taskl@gmail.com>
2021-08-25 23:42:19 +0700

    payment logic

--- trial-AnnyeongDog/Resources/AppDelegate.swift
+++ trial-AnnyeongDog/Resources/AppDelegate.swift
@@ -18,7 +18,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Override point for customization after application launch.
         FirebaseApp.configure()
         let db = Firestore.firestore()
-        
+        IAPManager.shared.fetchproducts()
         print(db)
         let user =  Auth.auth().currentUser
 
