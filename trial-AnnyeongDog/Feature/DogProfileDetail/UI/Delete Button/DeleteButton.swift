717c11704ae115ebb6c4990a6ca97759432913e7
zkake <ahmad.azka.kusuma@mail.ugm.ac.id>
2021-08-25 18:06:10 +0700

    refactoring folder and file

---
+++ trial-AnnyeongDog/Feature/DogProfileDetail/UI/Delete Button/DeleteButton.swift
@@ -0,0 +1,26 @@
+//
+//  DeleteButton.swift
+//  trial-AnnyeongDog
+//
+//  Created by Azka Kusuma on 25/08/21.
+//
+
+import UIKit
+
+class DeleteButton: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+    @IBAction func deleteProfilePress(_ sender: UIButton) {
+        
+    }
+}
